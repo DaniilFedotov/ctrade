b4109f57b7a9c21ca880eefd7c292fcbaca9c0c2
Daniil Fedotov <pustovdanil@bk.ru>
2024-02-01 14:46:19 +0500

    Add deal model

--- backend/deals/models.py
+++ backend/deals/models.py
@@ -1,1 +1,25 @@
 from django.db import models
+
+
+class Deal(models.Model):
+    """Model for deals."""
+    STATUS_CHOICES = (
+        ('C', 'Closed'),
+        ('P', 'In progress'),
+    )
+    id = models.AutoField(primary_key=True)
+    opening_date = models.DateField(
+        verbose_name='Trade opening date',
+        auto_now_add=True,
+    )
+    status = models.CharField(
+        verbose_name='Status of deal',
+        choices=STATUS_CHOICES,
+    )
+    purchase_price = models.FloatField(
+        verbose_name='Trade entry price',
+    )
+    selling_price = models.FloatField(
+        verbose_name='Trade exit price',
+        blank=True,
+    )
