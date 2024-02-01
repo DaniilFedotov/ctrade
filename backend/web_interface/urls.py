c8c08c190b03009f325b8f2a571c19afb8024cfd
Daniil Fedotov <pustovdanil@bk.ru>
2024-02-01 21:14:30 +0500

    add main urls

--- backend/web_interface/urls.py
+++ backend/web_interface/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('deals/', include('deals.urls', namespace='deals')),
+    path('traders/', include('traders.urls', namespace='traders')),
 ]
