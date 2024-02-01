685652cd6020c1b4e3ecab55cff21248dfcd8127
Daniil Fedotov <pustovdanil@bk.ru>
2024-02-01 22:34:59 +0500

    change main urls and setup deals urls

--- backend/deals/urls.py
+++ backend/deals/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
 
+from . import views
 
 app_name = 'deals'
+
+urlpatterns = [
+    path('deals/', views.deals, name='deals'),
+    path('traders/', views.traders, name='traders'),
+]
--- backend/web_interface/urls.py
+++ backend/web_interface/urls.py
@@ -3,6 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('deals/', include('deals.urls', namespace='deals')),
-    path('traders/', include('traders.urls', namespace='traders')),
+    path('', include('deals.urls', namespace='deals')),
 ]
