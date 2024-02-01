393a2b1c02611120e4f7673ca1122a89fb89bb1e
Daniil Fedotov <pustovdanil@bk.ru>
2024-02-01 22:19:24 +0500

    add views

--- backend/deals/views.py
+++ backend/deals/views.py
@@ -1,2 +1,19 @@
 from django.shortcuts import render
 
+from .models import Deal, Trader
+
+
+def deals(request):
+    deals_objects = Deal.objects.all()
+    context = {
+        'deals': deals_objects,
+    }
+    return render(request, 'deals/deals.html', context)
+
+
+def traders(request):
+    traders_objects = Trader.objects.all()
+    context = {
+        'traders': traders_objects,
+    }
+    return render(request, 'deals/traders.html', context)
