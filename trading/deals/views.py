3e0732c920230331b155fc4cffb65dcd8b71087a
Daniil Fedotov <pustovdanil@bk.ru>
2024-02-07 00:22:06 +0500

    add deal and trader viewsets

--- trading/deals/views.py
+++ trading/deals/views.py
@@ -1,1 +1,18 @@
 from django.shortcuts import render
+
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Deal, Trader
+from .serializers import DealSerializer, TraderSerializer
+
+
+class DealViewSet(ModelViewSet):
+    """Viewset for the deal model."""
+    queryset = Deal.objects.all()
+    serializer_class = DealSerializer
+
+
+class TraderViewSet(ModelViewSet):
+    """Viewset for the trader model."""
+    queryset = Trader.objects.all()
+    serializer_class = TraderSerializer
