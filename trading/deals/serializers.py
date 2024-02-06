ce4f3ff41219a033dededd202a38cb8f8250eaa5
Daniil Fedotov <pustovdanil@bk.ru>
2024-02-07 00:31:02 +0500

    add deal and trader serializers

---
+++ trading/deals/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework.serializers import ModelSerializer
+
+from .models import Deal, Trader
+
+
+class DealSerializer(ModelSerializer):
+    """Serializer for the deal model."""
+    class Meta:
+        model = Deal
+        fields = ('id', 'opening_date', 'closed', 'ticker',
+                  'purchase_price', 'selling_price', 'trader',)
+        read_only_fields = ('id',)
+
+
+class TraderSerializer(ModelSerializer):
+    """Serializer for the trader model."""
+    class Meta:
+        model = Trader
+        fields = ('id', 'creation_date', 'working', 'initial_deposit',
+                  'current_deposit', 'revenue', 'market',)
+        read_only_fields = ('id',)
