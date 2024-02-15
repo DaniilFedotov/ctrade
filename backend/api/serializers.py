a1b79add9066b10b8e9a1062d4bc44cff7202d43
Daniil Fedotov <pustovdanil@bk.ru>
2024-02-15 16:06:13 +0500

    add update-method for DealSerializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -9,8 +9,18 @@ class DealSerializer(ModelSerializer):
         model = Deal
         fields = ('id', 'opening_date', 'closed', 'ticker',
                   'quantity', 'purchase_price', 'selling_price',
-                  'trader',)
+                  'revenue', 'trader',)
         read_only_fields = ('id',)
+
+    def update(self, instance, validated_data):
+        quantity = instance.quantity
+        purchase_price = instance.purchase_price
+        selling_price = validated_data['selling_price']
+        instance.selling_price = selling_price
+        revenue = round(quantity * (selling_price - purchase_price), 2)
+        instance.revenue = revenue
+        instance.save()
+        return instance
 
 
 class TraderSerializer(ModelSerializer):
