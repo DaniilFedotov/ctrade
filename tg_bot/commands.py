7ef13f2111bfdbc2b385dffa7b120fc6476b5ef4
Daniil Fedotov <pustovdanil@bk.ru>
2024-02-20 19:44:25 +0500

    add new function, which get id of a running bots

--- tg_bot/commands.py
+++ tg_bot/commands.py
@@ -5,7 +5,6 @@ import requests
 
 def start_trading(update, context):
     """Launches the specified bot."""
-    chat = update.effective_chat
     command = update['message']['text'].split(' ')
     if len(command) == 2:
         bot_id = int(command[1])
@@ -13,7 +12,7 @@ def start_trading(update, context):
             f'http://localhost:8000/api/traders/{bot_id}/')
         if trader.json()['working']:
             context.bot.send_message(
-                chat_id=chat.id,
+                chat_id=update.effective_chat.id,
                 text=f'This bot is already running.')
         else:
             response = requests.patch(
@@ -21,21 +20,20 @@ def start_trading(update, context):
                 data={'working': True})
             if response.status_code == 200:
                 context.bot.send_message(
-                    chat_id=chat.id,
+                    chat_id=update.effective_chat.id,
                     text=f'The bot {bot_id} is running.')
             else:
                 context.bot.send_message(
-                    chat_id=chat.id,
+                    chat_id=update.effective_chat.id,
                     text=f'Unexpected error.')
     else:
         context.bot.send_message(
-            chat_id=chat.id,
+            chat_id=update.effective_chat.id,
             text=f'Bot number not specified.')
 
 
 def stop_trading(update, context):
     """Stops the specified bot."""
-    chat = update.effective_chat
     command = update['message']['text'].split(' ')
     if len(command) == 2:
         bot_id = int(command[1])
@@ -47,25 +45,24 @@ def stop_trading(update, context):
                 data={'working': False})
             if response.status_code == 200:
                 context.bot.send_message(
-                    chat_id=chat.id,
+                    chat_id=update.effective_chat.id,
                     text=f'The bot {bot_id} is stopped.')
             else:
                 context.bot.send_message(
-                    chat_id=chat.id,
+                    chat_id=update.effective_chat.id,
                     text=f'Unexpected error.')
         else:
             context.bot.send_message(
-                chat_id=chat.id,
+                chat_id=update.effective_chat.id,
                 text=f'This bot has already been stopped.')
     else:
         context.bot.send_message(
-            chat_id=chat.id,
+            chat_id=update.effective_chat.id,
             text=f'Bot number not specified.')
 
 
 def get_revenue(update, context):
     """Calculates revenue for recent transactions."""
-    chat = update.effective_chat
     command = update['message']['text'].split(' ')
     if len(command) == 2:
         num_of_deals = int(command[1])
@@ -78,13 +75,12 @@ def get_revenue(update, context):
         if deal['closed']:
             revenue += deal['revenue']
     context.bot.send_message(
-        chat_id=chat.id,
+        chat_id=update.effective_chat.id,
         text=f'Revenue for the last {num_of_deals} trades: {revenue} USD.')
 
 
 def get_daily_revenue(update, context):
     """Calculates revenue for today's transactions."""
-    chat = update.effective_chat
     deals = requests.get('http://localhost:8000/api/deals/')
     today = datetime.datetime.now().strftime('%Y-%m-%d')
     revenue = 0
@@ -95,5 +91,21 @@ def get_daily_revenue(update, context):
         else:
             break
     context.bot.send_message(
-        chat_id=chat.id,
+        chat_id=update.effective_chat.id,
         text=f'Revenue for today: {revenue} USD.')
+
+
+def get_bot_id(update, context):
+    """Give the id of a running trading bot."""
+    traders = requests.get('http://localhost:8000/api/traders/')
+    bot_id = None
+    for trader in traders.json():
+        if trader['working']:
+            bot_id = trader['id']
+            context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text=f'Id of a running trading bot: {bot_id}.')
+    if bot_id is None:
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=f'No running bots found.')
--- tg_bot/main.py
+++ tg_bot/main.py
@@ -4,7 +4,8 @@ from telegram import Bot
 from telegram.ext import Updater, Filters, MessageHandler, CommandHandler
 from dotenv import load_dotenv
 
-from commands import start_trading, stop_trading, get_revenue, get_daily_revenue
+from commands import (start_trading, stop_trading, get_revenue,
+                      get_daily_revenue, get_bot_id)
 
 
 load_dotenv()
@@ -16,7 +17,8 @@ HELP_TEXT = (f'Bot is running. You can use the following commands:\n'
              f'/start x - start existing trading bot with id x;\n'
              f'/stop x - stop trading bot with id x;\n'
              f'/revenue x - get revenue for the last x trades (default - 5);\n'
-             f'/daily - get revenue for today;\n')
+             f'/daily - get revenue for today;\n'
+             f'/id - get the id of a running bots;\n')
 
 bot = Bot(token=TELEGRAM_TOKEN)
 updater = Updater(token=TELEGRAM_TOKEN)
@@ -34,6 +36,7 @@ def main():
     updater.dispatcher.add_handler(CommandHandler('stop', stop_trading))
     updater.dispatcher.add_handler(CommandHandler('revenue', get_revenue))
     updater.dispatcher.add_handler(CommandHandler('daily', get_daily_revenue))
+    updater.dispatcher.add_handler(CommandHandler('id', get_bot_id))
     updater.dispatcher.add_handler(MessageHandler(Filters.text, get_help))
     updater.start_polling(poll_interval=1.0)
     updater.idle()
