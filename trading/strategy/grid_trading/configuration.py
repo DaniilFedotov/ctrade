68f995a40625c69cf6e1e1c896c9015a1741579b
Daniil Fedotov <pustovdanil@bk.ru>
2024-02-21 23:10:57 +0500

    create setup_grid function

---
+++ trading/strategy/grid_trading/configuration.py
@@ -0,0 +1,34 @@
+GRID_SETTINGS = {
+    'BTCUSDT': {
+        'middle': None,
+        'max': None,
+        'min': None,
+        'number_of_levels': None,
+        'deposit': None,
+        'precision': None,
+    },
+    'APTUSDT': {
+        'middle': None,
+        'max': None,
+        'min': None,
+        'number_of_levels': None,
+        'deposit': None,
+        'precision': None,
+    },
+    'SOLUSDT': {
+        'middle': None,
+        'max': None,
+        'min': None,
+        'number_of_levels': None,
+        'deposit': None,
+        'precision': None,
+    },
+    'ARBUSDT': {
+        'middle': None,
+        'max': None,
+        'min': None,
+        'number_of_levels': None,
+        'deposit': None,
+        'precision': None,
+    },
+}
--- trading/strategy/grid_trading/grid_trading.py
+++ trading/strategy/grid_trading/grid_trading.py
@@ -1,7 +1,24 @@
 import time
 import requests
 
+import configuration
+
 
 def trading(trading_bot):
+    grid = setup_grid(trading_bot.pair)
     while True:
         pass
+
+
+def setup_grid(pair):
+    grid_parameters = configuration.GRID_SETTINGS[pair]
+    delta = round(((grid_parameters['max'] - grid_parameters['min']) /
+                   (grid_parameters['number_of_levels'] - 1)),
+                  grid_parameters['precision'])
+    grid = {}
+    if grid_parameters['number_of_levels'] % 2 == 0:
+        for ind in range(grid_parameters['number_of_levels'] / 2):
+            grid[f'buy{ind}'] = grid_parameters['min'] + delta * ind
+        for ind in range(grid_parameters['number_of_levels'] / 2):
+            grid[f'sell{ind}'] = grid_parameters['max'] - delta * ind
+    return grid
