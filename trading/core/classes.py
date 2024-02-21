617b141e59139f61b96c1a0b2df742a7ec155554
Daniil Fedotov <pustovdanil@bk.ru>
2024-02-21 12:11:15 +0500

    add initial class for trading bot

---
+++ trading/core/classes.py
@@ -0,0 +1,43 @@
+from exchange import binance, bybit
+
+
+class InitialTrader:
+    def __init__(self, trader_id, token, currency, client, exchange):
+        self.trader_id = trader_id
+        self.token = token
+        self.currency = currency
+        self.pair = token + currency
+        self.client = client
+        self.exchange = exchange
+
+    def check_price(self):
+        """Checks the price of a coin."""
+        match self.exchange:
+            case 'binance':
+                return binance.check_price()
+            case 'bybit':
+                return bybit.check_price()
+
+    def get_balance(self):
+        """Checks the balance of a trader."""
+        match self.exchange:
+            case 'binance':
+                return binance.get_balance()
+            case 'bybit':
+                return bybit.get_balance()
+
+    def buy_coin(self):
+        """Places an order to buy a coin."""
+        match self.exchange:
+            case 'binance':
+                return binance.buy_coin()
+            case 'bybit':
+                return bybit.buy_coin()
+
+    def sell_coin(self):
+        """Places an order to sell a coin."""
+        match self.exchange:
+            case 'binance':
+                return binance.sell_coin()
+            case 'bybit':
+                return bybit.sell_coin()
---
+++ trading/exchange/bybit.py
@@ -0,0 +1,14 @@
+def get_balance():
+    pass
+
+
+def check_price():
+    pass
+
+
+def buy_coin():
+    pass
+
+
+def sell_coin():
+    pass
