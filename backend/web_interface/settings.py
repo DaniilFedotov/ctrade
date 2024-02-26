c4d98d081b21a0cb743a835c7a444772f631e3a8
Daniil Fedotov <pustovdanil@bk.ru>
2024-02-27 00:36:40 +0500

    working docker-compose

--- backend/web_interface/settings.py
+++ backend/web_interface/settings.py
@@ -58,14 +58,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'web_interface.wsgi.application'
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}"""
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', 'django'),
@@ -74,7 +74,7 @@ DATABASES = {
         'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', 5432),
     }
-}"""
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
