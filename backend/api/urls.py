3edb28ecdba49e36b9078a830bfafd588ae3bb9b
Daniil Fedotov <pustovdanil@bk.ru>
2024-02-29 14:22:44 +0500

    add new paths for urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import DealViewSet, TraderViewSet
+from .views import (DealViewSet, TraderViewSet,
+                    GridViewSet, LevelViewSet)
 
 app_name = 'api'
 
 router = DefaultRouter()
 router.register('deals', DealViewSet, basename='deals')
 router.register('traders', TraderViewSet, basename='traders')
+router.register('grids', GridViewSet, basename='grids')
+router.register('levels', LevelViewSet, basename='levels')
 
 urlpatterns = [
     path('', include(router.urls), name='api-root'),
