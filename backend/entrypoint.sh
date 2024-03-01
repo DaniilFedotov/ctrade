c3f10a6895efaf06f17a725dc608756af1fb8aeb
Daniil Fedotov <pustovdanil@bk.ru>
2024-03-01 12:32:58 +0500

    change entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,8 +4,5 @@ echo "&{0}: running migrations."
 python manage.py makemigrations
 python manage.py migrate
 
-echo "&{0}: running import of test data."
-python manage.py testimport
-
 echo "&{0}: running server."
 python manage.py runserver 0:8000
