0f6148df9c1c094648ea1093ead9319ff53ebe6a
Daniil Fedotov <pustovdanil@bk.ru>
2024-03-01 14:54:26 +0500

    entrypoint test version

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,5 +4,8 @@ echo "&{0}: running migrations."
 python manage.py makemigrations
 python manage.py migrate
 
+echo "&{0}: running import of test data."
+python manage.py testimport
+
 echo "&{0}: running server."
 python manage.py runserver 0:8000
