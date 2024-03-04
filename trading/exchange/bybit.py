7f583f5460d97feb44437b8686283999dfac4b13
Daniil Fedotov <pustovdanil@bk.ru>
2024-03-04 22:55:51 +0500

    add pybit

--- trading/exchange/bybit.py
+++ trading/exchange/bybit.py
@@ -1,3 +1,21 @@
+import os
+
+from pybit.unified_trading import HTTP
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+BYBIT_API_KEY = os.getenv('BYBIT_API_KEY')
+BYBIT_SECRET_KEY = os.getenv('BYBIT_SECRET_KEY')
+
+session = HTTP(
+    testnet=False,
+    api_key=BYBIT_API_KEY,
+    apy_secret=BYBIT_SECRET_KEY
+)
+
+
 def get_balance():
     pass
 
