37ce8b39b3893ac8d371e3c8299b8ae818e10c8e
Daniil Fedotov <pustovdanil@bk.ru>
2024-03-06 11:58:30 +0500

    add new management commands

---
+++ backend/deals/management/commands/importsettings.py
@@ -0,0 +1,41 @@
+import csv
+
+from django.core.management.base import BaseCommand
+
+from deals.models import (Trader, Grid, Token,
+                          Currency, TradingPair)
+
+PATH = 'static/data/settings/'
+FILES__MODELS = {
+    'tokens': Token,
+    'currencies': Currency,
+    'tradingpairs': TradingPair,
+    'grids': Grid,
+    'traders': Trader,
+}
+
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--delete-existing',
+            action='store_true',
+            dest='delete_existing',
+            default=False,
+        )
+
+    def handle(self, *args, **options):
+        for file, model in FILES__MODELS.items():
+            with open(PATH + f'{file}.csv', 'r', newline='') as csvfile:
+                reader = csv.DictReader(csvfile)
+                records = []
+                for row in reader:
+                    records.append(model(**row))
+                if options['delete_existing']:
+                    model.objects.all().delete()
+                    self.stdout.write(self.style.SUCCESS(
+                        'Previous data deleted.'))
+                model.objects.bulk_create(records)
+                self.stdout.write(self.style.SUCCESS(f'Data from {file}.scv imported.'))
+                csvfile.close()
