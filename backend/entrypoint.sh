1ca08270943c7a04895c4f50990405c9f31b0a8f
Daniil Fedotov <pustovdanil@bk.ru>
2024-03-23 20:34:45 +0500

    remove import of settings from entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,8 +4,5 @@ echo "&{0}: running migrations."
 python manage.py makemigrations
 python manage.py migrate
 
-echo "&{0}: running import of settings data."
-python manage.py importsettings
-
 echo "&{0}: running server."
 python manage.py runserver 0:8000
