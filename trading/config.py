353117f1ce2be77cece12dfa637e029f5459d77b
Daniil Fedotov <pustovdanil@bk.ru>
2024-08-13 01:07:43 +0500

    Add config file

---
+++ trading/config.py
@@ -0,0 +1,22 @@
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+BACKEND_URL = os.getenv("BACKEND_URL")
+BYBIT_API_KEY = os.getenv("BYBIT_API_KEY")
+BYBIT_SECRET_KEY = os.getenv("BYBIT_SECRET_KEY")
+CHECK_TIME_SEC = os.getenv("CHECK_TIME_SEC")
+SAFETY_FACTOR = os.getenv("SAFETY_FACTOR")
+SLEEPTIME_SEC = os.getenv("SLEEPTIME_SEC")
+MINIMUM_ORDER_SIZE = os.getenv("MINIMUM_ORDER_SIZE")
+
+api_requests_mapping = {
+    "traders": "/api/traders",
+    "deals": "/api/deals",
+    "levels": "/api/levels",
+    "grids": "/api/grids",
+    "tickers": "/api/tickers"
+}
