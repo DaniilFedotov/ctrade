9df26cf2ef309778e00338a88f9d4f92a487ecf5
Daniil Fedotov <pustovdanil@bk.ru>
2024-09-29 00:07:14 +0500

    Add RECV_WINDOW and some types of variables

--- trading/config.py
+++ trading/config.py
@@ -7,11 +7,12 @@ load_dotenv()
 
 BACKEND_URL = os.getenv("BACKEND_URL")
 BYBIT_API_KEY = os.getenv("BYBIT_API_KEY")
+BYBIT_RECV_WINDOW = int(os.getenv("BYBIT_RECV_WINDOW"))
 BYBIT_SECRET_KEY = os.getenv("BYBIT_SECRET_KEY")
-CHECK_TIME_SEC = os.getenv("CHECK_TIME_SEC")
-SAFETY_FACTOR = os.getenv("SAFETY_FACTOR")
-MINIMUM_ORDER_SIZE = os.getenv("MINIMUM_ORDER_SIZE")
-WAITING_TIME_SEC = os.getenv("WAITING_TIME_SEC")
+CHECK_TIME_SEC = float(os.getenv("CHECK_TIME_SEC"))
+SAFETY_FACTOR = float(os.getenv("SAFETY_FACTOR"))
+MINIMUM_ORDER_SIZE = float(os.getenv("MINIMUM_ORDER_SIZE"))
+WAITING_TIME_SEC = float(os.getenv("WAITING_TIME_SEC"))
 
 api_requests_mapping = {
     "traders": "/api/traders",
