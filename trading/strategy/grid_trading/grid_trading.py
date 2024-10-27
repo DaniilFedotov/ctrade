1bc22275aa569df8418dbfe23f002c688b36a00a
Daniil Fedotov <pustovdanil@bk.ru>
2024-10-27 19:45:41 +0500

    Change trading_process logic, optimize checking orders

--- trading/strategy/grid_trading/grid_trading.py
+++ trading/strategy/grid_trading/grid_trading.py
@@ -1,46 +1,34 @@
 import logging
 import time
 
-from config import CHECK_TIME_SEC
+from config import CHECK_TIME_SEC, MINIMAL_CHECK_TIME_SEC
 from core.classes import TradingBot
 from core.managers import DealManager, GridManager, LevelManager, TraderManager
-from strategy.grid_trading.utils import install_grid, update_state
+from strategy.grid_trading.utils import install_grid, update_state, remove_grid
 
 
 logger = logging.getLogger(__name__)
 
 
-def trading_process(trading_bot: TradingBot):
-    """Main trading function."""
-    logger.debug("Start trading")
-    trader = TraderManager.get_trader(
-        trader_id=trading_bot.trader_id
-    )
-    logger.debug(f"Trader: {trader}")
-    if not trader["initial_deposit"]:
-        logger.debug("Get initial_deposit")
-        balance = trading_bot.get_balance()
-        TraderManager.update_trader(
-            trader_id=trading_bot.trader_id,
-            trader_data={
-                "initial_deposit": balance,
-                "current_deposit": balance
-            }
-        )
-    grid_installed = install_grid(trading_bot)
-    logger.debug(f"Grid installed : {grid_installed}")
-    while grid_installed:
-        logger.debug("Start checking orders")
-        trader = TraderManager.get_trader(
-            trader_id=trading_bot.trader_id
-        )
-        logger.debug(f"Trader: {trader}")
-        if not trader["working"]:
-            logger.debug(f"Trader status: {trader['working']}")
-            break
-        cur_grid = trader["grid"]
-        cur_levels = trader["grid"]["levels"]
-        for level in cur_levels:
+def check_orders(trader: dict, trading_bot: TradingBot):
+    """Checks orders statuses."""
+    logger.debug("Start checking orders")
+    cur_grid = trader["grid"]
+    cur_levels = trader["grid"]["levels"]
+    orders_info = trading_bot.get_open_orders()
+    logger.debug(f"orders_info: {orders_info}")
+    for level in cur_levels:
+        order_status = None
+        for ind, order in enumerate(orders_info):
+            if level["order_id"] == order["orderId"]:
+                order_status = order["orderStatus"]
+                orders_info.pop(ind)
+                break
+        logger.debug(f"Order id: {level['order_id']}"
+                     f"Order status: {order_status}")
+        if order_status == "New":
+            continue
+        elif order_status is None:
             order_info = trading_bot.get_order(
                 order_id=level["order_id"],
                 closed=True
@@ -48,7 +36,6 @@ def trading_process(trading_bot: TradingBot):
             logger.debug(f"Order info: {order_info}")
             if order_info:
                 order_status = order_info[0]["orderStatus"]
-                logger.debug(f"Order status: {order_status}")
                 if order_status == "Filled":
                     logger.debug("Find filled order")
                     if level["inverse"]:
@@ -106,7 +93,56 @@ def trading_process(trading_bot: TradingBot):
                     )
                     logger.debug(f"Next level: {next_level}")
                     trading_bot = update_state(trading_bot)
-        time.sleep(CHECK_TIME_SEC)
-        grid_installed = GridManager.get_grid(
-            grid_id=trading_bot.grid["id"]
-        )["installed"]
+            else:
+                order_info = trading_bot.get_order(
+                    order_id=level["order_id"],
+                )
+                if order_info:
+                    order_status = order_info[0]["orderStatus"]
+                    if order_status == "New":
+                        continue
+                logger.debug("Order not found")
+                continue
+
+
+def trading_process(trading_bot: TradingBot):
+    """Main trading function."""
+    logger.debug("Start trading")
+    trader = TraderManager.get_trader(
+        trader_id=trading_bot.trader_id
+    )
+    logger.debug(f"Trader: {trader}")
+    if not trader["initial_deposit"]:
+        logger.debug("Get initial_deposit")
+        balance = trading_bot.get_balance()
+        TraderManager.update_trader(
+            trader_id=trading_bot.trader_id,
+            trader_data={
+                "initial_deposit": balance,
+                "current_deposit": balance
+            }
+        )
+    while trader["working"]:
+
+        cur_price = trading_bot.trading_pair.value_formatting(
+            value=trading_bot.check_price(),
+            parameter="price"
+        )
+
+        if trader["grid"]["bottom"] <= cur_price <= trader["grid"]["top"]:
+            if not trader["grid"]["installed"]:
+                install_grid(trading_bot)
+            check_orders(
+                trader=trader,
+                trading_bot=trading_bot
+            )
+            time.sleep(CHECK_TIME_SEC)
+        else:
+            if trader["grid"]["installed"]:
+                remove_grid(trading_bot)
+            time.sleep(MINIMAL_CHECK_TIME_SEC)
+
+        trader = TraderManager.get_trader(
+            trader_id=trading_bot.trader_id
+        )
+        logger.debug(f"Trader: {trader}")
