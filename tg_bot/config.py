4f1551dba0d6f8659b3b05a8719543868ac5a593
Daniil Fedotov <pustovdanil@bk.ru>
2024-12-01 01:06:56 +0500

    Add list of available exchanges

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -6,9 +6,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BACKEND_URL = os.getenv("BACKEND_URL")
-MSG_MAX_SIZE_PC = os.getenv("MSG_MAX_SIZE_PC")
+MSG_MAX_SIZE_PC = int(os.getenv("MSG_MAX_SIZE_PC"))
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+AVAILABLE_EXCHANGES = ["binance", "bybit"]
 
 api_requests_mapping = {
     "traders": "/api/traders",
